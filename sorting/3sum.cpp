81e099cecfed4062bcf386e31b70783b522d8704
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-08-11 11:42:36 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.cpp
+++ sorting/3sum.cpp
@@ -6,6 +6,7 @@ public:
         vector<vector<int>> ans;
 
         for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int l = i+1;
             int k = n-1;
             while(l<k){
@@ -17,7 +18,7 @@ public:
                     l++;
                     k--;
                 }
-                if(sum<0) l++;
+                else if(sum<0) l++;
                 else k--;
             }
         }
