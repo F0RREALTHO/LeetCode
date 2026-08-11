524969255c46ab0c3ac381e18bed354d7f86c41e
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-08-11 11:40:07 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

---
+++ array/3sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+
+        for(int i=0; i<n; i++){
+            int l = i+1;
+            int k = n-1;
+            while(l<k){
+                int sum = nums[i]+nums[l]+nums[k];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[l],nums[k]});
+                    if(l<k && nums[l]==nums[l+1]) l++;
+                    if(l<k && nums[k]==nums[k-1]) k--; 
+                    l++;
+                    k--;
+                }
+                if(sum<0) l++;
+                else k--;
+            }
+        }
+        return ans;
+    }
+};
