d78744b7611fe5d88a346b7094dc9f9ff7cf9f79
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-08-22 11:26:28 +0530

    LC-integer-to-roman [Runtime: N/A; Memory: N/A]

--- math/integer-to-roman.cpp
+++ math/integer-to-roman.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        vector<int> val = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        vector<string> roman = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
         string ans = "";
-        int cnt = 0;
-        while(num>=1000 && cnt!=3){
-            ans+="M";cnt++; num-=1000;
+
+        for (int i = 0; i < 13; i++) {
+            while (num >= val[i]) {
+                ans += roman[i];
+                num -= val[i];
+            }
         }
-        cnt=0;
-        if(num>=900){
-            ans+="CM";
-            num-=900;
-        }
-        if(num>=500){
-            ans+="D";num-=500;
-        }
-        if(num>=400){
-            ans+="CD"; num-=400;
-        }
-        while(num>=100 && cnt!=3){
-            ans+="C";cnt++;num-=100;
-        }
-        cnt=0;
-        if(num>=90){
-            ans+="XC";num-=90;
-        }
-        if(num>=50){
-            ans+="L";num-=50;
-        }
-        if(num>=40){
-            ans+="XL"; num-=40;
-        }
-        while(num>=10 && cnt!=3){
-            ans+="X";cnt++;num-=10;
-        }
-        if(num>=9){
-            ans+="IX";num-=9;
-        }
-        if(num>=5){
-            ans+="V";num-=5;
-        }
-        if(num>=4){
-            ans+="IV";num-=4;
-        }
-        while(num>=1){
-            ans+="I"; num-=1;
-        }
+
         return ans;
     }
 };
