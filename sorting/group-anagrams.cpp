5fc639e7a9047b148e2808012fc5a3f9ea7cf996
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-08-26 19:19:52 +0530

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

---
+++ sorting/group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> ans;
+        for(auto &s:strs){
+            vector<int> cnt(26,0);
+            for(char c:s){
+                cnt[c-'a']++;
+            }
+            string k;
+            for(int x:cnt){
+                k+= to_string(x);
+            }
+            ans[k].push_back(s);
+        }
+        vector<vector<string>> res;
+        for(auto &s:ans){
+            res.push_back(s.second);
+        }
+        return res;
+    }
+};
