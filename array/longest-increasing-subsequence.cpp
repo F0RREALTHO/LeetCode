81558b203ef30a88ee02408e5cb19a597fa2f797
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-09-02 22:04:05 +0530

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

---
+++ array/longest-increasing-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,1);
+        for(int i =0; i<n; i++){
+            for(int k=0;k<i;k++){
+                if(nums[k]<nums[i]) dp[i] = max(dp[i],dp[k]+1);
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
