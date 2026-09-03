9f3269fe8ac1c20dfdcace7c72d13b21f13c014e
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-09-03 12:53:03 +0530

    LC-word-search [Runtime: N/A; Memory: N/A]

--- depth-first-search/word-search.cpp
+++ depth-first-search/word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool f(string s, int x, int y vector<vector<char>>& board, string word, vector<vector<bool>>& vis, int n, int m, vector<pair<int,int>>& dir){
+    bool f(string s, int x, int y, vector<vector<char>>& board, string word, vector<vector<bool>>& vis, int n, int m, vector<pair<int,int>>& dir){
         if(s==word){
             return true;
         }
@@ -10,7 +10,7 @@ public:
             if(nx>=0 && nx<m && ny>=0 && ny<n && !vis[nx][ny]){
                 s+=board[nx][ny];
                 vis[nx][ny]=true;
-                f(s,nx,ny,board,word,vis,n,m,dir);
+                if(f(s,nx,ny,board,word,vis,n,m,dir)) return true;
                 s.pop_back();
                 vis[nx][ny]=false;
             }
@@ -21,11 +21,16 @@ public:
     }
     bool exist(vector<vector<char>>& board, string word) {
         vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
-        int m= baord.size();
+        int m= board.size();
         int n = board[0].size();
+        vector<vector<bool>> vis (m,vector<bool>(n,false));
         for(int i =0; i<m; i++){
             for(int j =0;j<n;j++){
+                string s ="";
+                s+=board[i][j];
+                vis[i][j]= true;
                 if(f(s,i,j,board,word,vis,n,m,dir)==true) return true;
+                vis[i][j]=false;
             }
         }
         return false;
