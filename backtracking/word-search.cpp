05c86ddb6c29ff2d3dc9770a7caf97f03753e724
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-09-03 13:07:26 +0530

    LC-word-search [Runtime: N/A; Memory: N/A]

--- backtracking/word-search.cpp
+++ backtracking/word-search.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    bool f(string s, int x, int y, vector<vector<char>>& board, string word, vector<vector<bool>>& vis, int n, int m, vector<pair<int,int>>& dir){
+    bool f(string s, int x, int y, vector<vector<char>>& board, string word, int n, int m, vector<pair<int,int>>& dir){
         if(s.size()==word.size()){
             return true;
         }
         // s+= board[x][y];
         for(auto [dx,dy]:dir){
             int nx = x+dx, ny =y+dy;
-            if(nx>=0 && nx<m && ny>=0 && ny<n && !vis[nx][ny] && board[nx][ny] == word[s.size()]){
+            if(nx>=0 && nx<m && ny>=0 && ny<n && board[nx][ny]!='*' && board[nx][ny] == word[s.size()]){
                 s+=board[nx][ny];
-                vis[nx][ny]=true;
-                if(f(s,nx,ny,board,word,vis,n,m,dir)) return true;
+                char temp = board[nx][ny];
+                board[nx][ny] = '*';
+                if(f(s,nx,ny,board,word,n,m,dir)) return true;
                 s.pop_back();
-                vis[nx][ny]=false;
+                board[nx][ny]=temp;
             }
         }
 
@@ -23,15 +24,16 @@ public:
         vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
         int m= board.size();
         int n = board[0].size();
-        vector<vector<bool>> vis (m,vector<bool>(n,false));
+
         for(int i =0; i<m; i++){
             for(int j =0;j<n;j++){
                 if(board[i][j]!=word[0]) continue;
                 string s ="";
                 s+=board[i][j];
-                vis[i][j]= true;
-                if(f(s,i,j,board,word,vis,n,m,dir)==true) return true;
-                vis[i][j]=false;
+                char temp = board[i][j];
+                board[i][j]='*';
+                if(f(s,i,j,board,word,n,m,dir)==true) return true;
+                board[i][j]= temp;
             }
         }
         return false;
