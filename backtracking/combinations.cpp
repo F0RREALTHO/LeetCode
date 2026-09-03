7e8f0f1a13400430c293db3eff1b6eb43b9aa33f
Kartikeya Aryam <kartikeyaaryam7@gmail.com>
2026-09-03 10:51:34 +0530

    LC-combinations [Runtime: N/A; Memory: N/A]

--- backtracking/combinations.cpp
+++ backtracking/combinations.cpp
@@ -5,7 +5,6 @@ public:
             ans.push_back(sbq);
             return;
         }
-        if(num>n) return; 
         if(num<=n){
             sbq.push_back(num);
             f(n,k,num+1,sbq,ans);
